bf5d7386518072c57543e4472e7940b98a7fbf3b
HA0N1 <intor48@gmail.com>
2024-01-23 11:28:03 +0900

    tnwjd7

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 3000;
 connect();
 //몽고 db를 연결하기 위한 connect함수 실행.
 // json형태로 서버에 body 데이터를 전달하면,req.body에 데이터를 변환하여 넣어준다.
