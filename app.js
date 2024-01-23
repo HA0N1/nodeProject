f052618b88f6d817fb5632acae116e02264ee721
HA0N1 <intor48@gmail.com>
2024-01-23 11:03:28 +0900

    수정

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
-connect(); //몽고 db를 연결하기 위한 connect함수 실행.
+mongoose.connect(); //몽고 db를 연결하기 위한 connect함수 실행.
 // json형태로 서버에 body 데이터를 전달하면,req.body에 데이터를 변환하여 넣어준다.
 app.use(express.json());
 // form content type 에서 body 데이터를 전달하면, req.body에 데이터를 변환하여 넣어준다.
